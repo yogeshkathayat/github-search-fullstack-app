76ae694962b8ab301667a48880575ffecb99b039
Yogesh <kathayat.yogesh@gmail.com>
2020-08-17 23:58:54 +0400

    Updates

---
+++ precache-manifest.6900849fb7ce75074b5e21a8bea4db6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638592f7ef135dfde316ec0821e66c84",
+    "url": "/github-search-fullstack-app/index.html"
+  },
+  {
+    "revision": "ea67f21f0a8e24d8780d",
+    "url": "/github-search-fullstack-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2f2ab4e859006c4ad6fc",
+    "url": "/github-search-fullstack-app/static/js/2.51932240.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/github-search-fullstack-app/static/js/2.51932240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea67f21f0a8e24d8780d",
+    "url": "/github-search-fullstack-app/static/js/main.cf048c7c.chunk.js"
+  },
+  {
+    "revision": "c31625f185fc5c987c94",
+    "url": "/github-search-fullstack-app/static/js/runtime-main.df7070b1.js"
+  }
+]);
